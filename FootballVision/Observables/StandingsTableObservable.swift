3e6c2afb9b4a2d6a8d24988efd342977c7e965a2
Aryan Jagarwal <ajbaggar@gmail.com>
2023-07-27 21:26:40 +0530

    project setup

---
+++ FootballVision/Observables/StandingsTableObservable.swift
@@ -0,0 +1,8 @@
+//
+//  StandingsTableObservable.swift
+//  FootballVision
+//
+//  Created by Aryan Jagarwal on 27/07/23.
+//
+
+import Foundation
---
+++ FootballVision/Views/StandingsTableView.swift
@@ -0,0 +1,18 @@
+//
+//  StandingsTableView.swift
+//  FootballVision
+//
+//  Created by Aryan Jagarwal on 27/07/23.
+//
+
+import SwiftUI
+
+struct StandingsTableView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    StandingsTableView()
+}
