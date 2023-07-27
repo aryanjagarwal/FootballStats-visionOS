55ce741563f01328e298d5264b1a3b25f04c10a8
Aryan Jagarwal <ajbaggar@gmail.com>
2023-07-27 21:06:08 +0530

    Initial Commit

---
+++ FootballVision/FootballVisionApp.swift
@@ -0,0 +1,17 @@
+//
+//  FootballVisionApp.swift
+//  FootballVision
+//
+//  Created by Aryan Jagarwal on 27/07/23.
+//
+
+import SwiftUI
+
+@main
+struct FootballVisionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
