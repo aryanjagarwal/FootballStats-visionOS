0155f288606abf414cb11e6dd5dabc6112727394
Aryan Jagarwal <ajbaggar@gmail.com>
2023-08-03 09:56:09 +0530

    standings TableView

--- FootballVision/Views/StandingsTableView.swift
+++ FootballVision/Views/StandingsTableView.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import SVGImageSwiftUI
 import XCAFootballDataClient
 
 struct StandingsTableView: View {
@@ -16,9 +17,29 @@ struct StandingsTableView: View {
     var body: some View {
         Table(of: TeamStandingTable.self) {
             
-            TableColumn("Club") { team in
+            TableColumn("Club") { club in
                 HStack {
-                    Text(team.positionText).fontWeight(.bold)
+                    Text(club.positionText).fontWeight(.bold).frame(minWidth: 20)
+                    
+                    if let crest = club.team.crest, crest.hasSuffix("svg") {
+                        SVGImage(url: URL(string: crest)!, size: .init(width: 40, height: 40))
+                            .frame(width: 40, height: 40)
+                            .cornerRadius(20)
+                    } else {
+                        AsyncImage(url: URL(string:
+                            club.team.crest ?? "")) {phase
+                            in
+                            switch phase {
+                            case .success(let image):
+                                image.resizable()
+                            default: Circle().foregroundStyle(Color.gray.opacity(0.5))
+                            }
+                            
+                        }
+                            .frame(width: 40, height: 40)
+                    }
+                    
+                    Text(club.team.name)
                 }
             }
             .width(min: 264)
@@ -51,9 +72,41 @@ struct StandingsTableView: View {
                 Text($0.pointsText).frame(minWidth: 40) }
                 .width(40)
             
+            TableColumn("Last 5") {club in
+                HStack(spacing: 4) {
+                    if let formArray = club.formArray, !formArray.isEmpty {
+                        ForEach(formArray, id: \.self) {form in
+                            switch form {
+                            case "W":
+                                Image(systemName: "checkmark.circle.fill")
+                                    .symbolRenderingMode(.palette)
+                                    .foregroundStyle(Color.white, Color.green)
+                            case "L":
+                                Image(systemName: "xmark.circle.fill")
+                                    .symbolRenderingMode(.palette)
+                                    .foregroundStyle(Color.white, Color.red)
+                            default:
+                                Image(systemName: "minus.circle.fill")
+                                    .symbolRenderingMode(.palette)
+                                    .foregroundStyle(Color.white, Color.white.opacity(0.5))
+                            }
+                        }
+                    } else {
+                        Text("-").frame(width: 120, alignment: .center)
+                    }
+                }
+            }.width(120)
+            
         } rows: {
             ForEach(vm.standings ?? []) {
                 TableRow($0)
+            }
+        }
+        .overlay {
+            switch vm.fetchPhase {
+            case .fetching: ProgressView()
+            case .failure(let error): Text(error.localizedDescription).font(.headline)
+            default: EmptyView()
             }
         }
         .foregroundStyle(Color.primary)
